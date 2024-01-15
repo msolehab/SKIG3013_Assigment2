b3166dacb4c4944ac77bdc0537495a784589222b
pallyNaim <fadzli_naim_j@soc.uum.edu.my>
2024-01-15 21:54:10 +0800

    fadzli part

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+
+
+@app.route('/')  # Homepage
+def home():
+    return render_template('index.html')
+
+
+@app.route('/hobby')  # Hobby
+def hobby():
+    return render_template('hobby.html')
+
+
+@app.route('/portfolio')  # Portfolio
+def portfolio():
+    return render_template('portfolio.html')
+
+
+@app.route('/contact')  # Contact
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
